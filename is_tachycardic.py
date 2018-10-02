729dc467e508408873d10a3210453ec6a5df0035
Clark Bulleit <clark.bulleit@duke.edu>
2018-10-02 08:56:42 -0400

    makes is_tachycaridc.py conform to pep8

--- is_tachycardic.py
+++ is_tachycardic.py
@@ -1,10 +1,11 @@
 
-def is_tachycardic(candidate)
+def is_tachycardic(candidate):
+
     clean_candidate = candidate.strip().lower()
     clean_candidate = ''.join(letter for letter
                               in clean_candidate if letter.isalpha())
 
     if clean_candidate == 'tachycardic':
-            return true
+            return True
 
-    return false
+    return False
